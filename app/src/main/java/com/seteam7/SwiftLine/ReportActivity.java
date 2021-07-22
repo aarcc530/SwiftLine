6ecc1b65ea5be32d76f6b88cc92d916d7a508ab7
aarcc530 <aidan.casey@aarcc530.com>
2021-07-22 16:09:11 -0400

    fixed screen and added first set of color swaps

--- app/src/main/java/com/seteam7/SwiftLine/ReportActivity.java
+++ app/src/main/java/com/seteam7/SwiftLine/ReportActivity.java
@@ -1,8 +1,9 @@
 package com.seteam7.SwiftLine;
 
 import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
-import android.view.View;
+import android.util.TypedValue;
 import android.widget.Button;
 import android.widget.ImageButton;
 
@@ -19,45 +20,137 @@ public class ReportActivity extends AppCompatActivity {
     ImageButton team1;
     ImageButton team2;
     ImageButton backButton;
-    String storage1;
-    int storage2;
+    String teamStorage;
+    int waitTimeStorage;
     String mapsID;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_report);
 
-        fiveMin = (Button) findViewById(R.id.button2);
-        tenMin = (Button) findViewById(R.id.button3);
-        fifteenMin = (Button) findViewById(R.id.button4);
-        twentyMin = (Button) findViewById(R.id.button5);
-        thirtyMin = (Button) findViewById(R.id.button6);
-        fortyFiveMin = (Button) findViewById(R.id.button7);
-        submit = (Button) findViewById(R.id.button8);
-        team1 = (ImageButton) findViewById(R.id.imageButton);
-        team2 = (ImageButton) findViewById(R.id.imageButton2);
+        fiveMin = (Button) findViewById(R.id.Button5Minutes);
+        tenMin = (Button) findViewById(R.id.Button10Minutes);
+        fifteenMin = (Button) findViewById(R.id.Button15Minutes);
+        twentyMin = (Button) findViewById(R.id.Button20Minutes);
+        thirtyMin = (Button) findViewById(R.id.Button30Minutes);
+        fortyFiveMin = (Button) findViewById(R.id.Button45Minutes);
+        submit = (Button) findViewById(R.id.submitReportButton);
+        team1 = (ImageButton) findViewById(R.id.team1Button);
+        team2 = (ImageButton) findViewById(R.id.team2Button);
         backButton = (ImageButton) findViewById(R.id.RefreshButton);
 
         //TODO
 
-        team1.setOnClickListener(v -> storage1 = "team1");
+        team1.setOnClickListener(v -> {
+            teamStorage = "team1";
+        });
 
-        team2.setOnClickListener(v -> storage1 = "team2");
+        team2.setOnClickListener(v -> {
+            teamStorage = "team2";
+        });
 
-        fiveMin.setOnClickListener(v -> storage2 = 5);
+        fiveMin.setOnClickListener(v -> {
+            waitTimeStorage = 5;
+            TypedValue typedValue = new TypedValue();
+            Resources.Theme theme = getApplicationContext().getTheme();
+            theme.resolveAttribute(R.attr.colorPrimary, typedValue, true);
+            int primary = typedValue.data;
+            theme.resolveAttribute(R.attr.colorPrimaryVariant, typedValue, true);
+            int primaryVariant = typedValue.data;
+            fiveMin.setBackgroundColor(primaryVariant);
+            tenMin.setBackgroundColor(primary);
+            fifteenMin.setBackgroundColor(primary);
+            twentyMin.setBackgroundColor(primary);
+            thirtyMin.setBackgroundColor(primary);
+            fortyFiveMin.setBackgroundColor(primary);
+        });
 
-        tenMin.setOnClickListener(v -> storage2 = 10);
+        tenMin.setOnClickListener(v -> {
+            waitTimeStorage = 10;
+            TypedValue typedValue = new TypedValue();
+            Resources.Theme theme = getApplicationContext().getTheme();
+            theme.resolveAttribute(R.attr.colorPrimary, typedValue, true);
+            int primary = typedValue.data;
+            theme.resolveAttribute(R.attr.colorPrimaryVariant, typedValue, true);
+            int primaryVariant = typedValue.data;
+            fiveMin.setBackgroundColor(primary);
+            tenMin.setBackgroundColor(primaryVariant);
+            fifteenMin.setBackgroundColor(primary);
+            twentyMin.setBackgroundColor(primary);
+            thirtyMin.setBackgroundColor(primary);
+            fortyFiveMin.setBackgroundColor(primary);
+        });
 
-        fifteenMin.setOnClickListener(v -> storage2 = 15);
+        fifteenMin.setOnClickListener(v -> {
+            waitTimeStorage = 15;
 
-        twentyMin.setOnClickListener(v -> storage2 = 20);
+            TypedValue typedValue = new TypedValue();
+            Resources.Theme theme = getApplicationContext().getTheme();
+            theme.resolveAttribute(R.attr.colorPrimary, typedValue, true);
+            int primary = typedValue.data;
+            theme.resolveAttribute(R.attr.colorPrimaryVariant, typedValue, true);
+            int primaryVariant = typedValue.data;
+            fiveMin.setBackgroundColor(primary);
+            tenMin.setBackgroundColor(primary);
+            fifteenMin.setBackgroundColor(primaryVariant);
+            twentyMin.setBackgroundColor(primary);
+            thirtyMin.setBackgroundColor(primary);
+            fortyFiveMin.setBackgroundColor(primary);
+        });
 
-        thirtyMin.setOnClickListener(v -> storage2 = 30);
+        twentyMin.setOnClickListener(v -> {
+            waitTimeStorage = 20;
 
-        fortyFiveMin.setOnClickListener(v -> storage2 = 45);
+            TypedValue typedValue = new TypedValue();
+            Resources.Theme theme = getApplicationContext().getTheme();
+            theme.resolveAttribute(R.attr.colorPrimary, typedValue, true);
+            int primary = typedValue.data;
+            theme.resolveAttribute(R.attr.colorPrimaryVariant, typedValue, true);
+            int primaryVariant = typedValue.data;
+            fiveMin.setBackgroundColor(primary);
+            tenMin.setBackgroundColor(primary);
+            fifteenMin.setBackgroundColor(primary);
+            twentyMin.setBackgroundColor(primaryVariant);
+            thirtyMin.setBackgroundColor(primary);
+            fortyFiveMin.setBackgroundColor(primary);
+        });
+
+        thirtyMin.setOnClickListener(v -> {
+            waitTimeStorage = 30;
+
+            TypedValue typedValue = new TypedValue();
+            Resources.Theme theme = getApplicationContext().getTheme();
+            theme.resolveAttribute(R.attr.colorPrimary, typedValue, true);
+            int primary = typedValue.data;
+            theme.resolveAttribute(R.attr.colorPrimaryVariant, typedValue, true);
+            int primaryVariant = typedValue.data;
+            fiveMin.setBackgroundColor(primary);
+            tenMin.setBackgroundColor(primary);
+            fifteenMin.setBackgroundColor(primary);
+            twentyMin.setBackgroundColor(primary);
+            thirtyMin.setBackgroundColor(primaryVariant);
+            fortyFiveMin.setBackgroundColor(primary);
+        });
+
+        fortyFiveMin.setOnClickListener(v -> {
+            waitTimeStorage = 45;
+
+            TypedValue typedValue = new TypedValue();
+            Resources.Theme theme = getApplicationContext().getTheme();
+            theme.resolveAttribute(R.attr.colorPrimary, typedValue, true);
+            int primary = typedValue.data;
+            theme.resolveAttribute(R.attr.colorPrimaryVariant, typedValue, true);
+            int primaryVariant = typedValue.data;
+            fiveMin.setBackgroundColor(primary);
+            tenMin.setBackgroundColor(primary);
+            fifteenMin.setBackgroundColor(primary);
+            twentyMin.setBackgroundColor(primary);
+            thirtyMin.setBackgroundColor(primary);
+            fortyFiveMin.setBackgroundColor(primaryVariant);
+        });
 
         submit.setOnClickListener(v -> {
-            DatabaseCtl.sendReport(storage1, storage2, mapsID);
+            DatabaseCtl.sendReport(teamStorage, waitTimeStorage, mapsID);
             Intent intent = new Intent(ReportActivity.this, MapsActivity.class);
             startActivity(intent);
         });
